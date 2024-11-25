31f07714ab2e84403773fb85e01d0fc79747e351
Sam Neaves <samneaves@mac.home>
2024-11-25 11:06:07 +0000

    Add tangled file to get network

---
+++ scripts/get_genelist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# [[file:../readme.org::*RAS MAPK pathway][RAS MAPK pathway:1]]
+cat ../data/initial_gene_list_files/raas_genes.txt
+# RAS MAPK pathway:1 ends here
+
+# [[file:../readme.org::*RAS MAPK pathway][RAS MAPK pathway:2]]
+cat ./data/initial_gene_list_files/DopamineKEGGpathway.txt
+# RAS MAPK pathway:2 ends here
